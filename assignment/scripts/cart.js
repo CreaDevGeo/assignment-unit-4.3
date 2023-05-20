ed280da31b1bfd4d1bedfa772d2d3e1282557041
Georgio Harris <georgie.to.harris@gmail.com>
2023-05-19 23:17:19 -0500

    Tweaked code of stretch goals and manditory tasks. Currently adjusting code for empty to remove array spaces. Creating a working function that determines whether or not an array is full.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,82 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// **** SHOPPING CART PROGRAM (Grocery Store) ****
+
+// The cart/basket, items will be added to.
+let basket =[];
+
+
+// Adding items to basket.
+function addItem (item){
+    basket.push(item);
+    console.log("You added", item, "to the basket.")
+    return true;
+} // end addItem function
+
+// addItem function testing.
+console.log("*** - TEST - for function addItem. ***");
+console.log(addItem('Bananas'));
+console.log(addItem('Apples'));
+console.log(addItem('Salmon'));
+console.log(addItem('Chicken Breast'));
+console.log(addItem('Animal Craackers'));
+console.log(basket);
+
+
+// Listing each individual item in basket.
+function listItems(array){
+    console.log("The items in the basket are:")
+    for(i = 0; i < array.length; i ++){
+     console.log(array[i]); 
+}// end array item log
+return true;
+} // end listItems function
+
+// listItems function testing.
+console.log("*** - TEST - for function listItems. Expect listed items. ***");
+console.log(listItems(basket));
+console.log(basket);
+
+
+// Empty basket of all items
+function empty(array){
+    if(array === array){
+        array.length = 0;
+        array.pop(array.length - 1)
+        console.log("The basket is empty:", array);
+    } // end empty basket
+    return true;
+}// end empty function
+
+// empty function testing.
+console.log("*** - TEST - for function empty. Expect empty array. ***");
+console.log(empty(basket));
+console.log(basket);
+
+
+// **** Stretch Goal Portion ****
+
+const maxItems = [5];
+
+// Function to determine if array is full
+function itsFull(array){
+    if(array.length > maxItems){
+         console.log("Your basket is over-flowingly full, remove some items!");
+         return true;
+    } // end it's overfilled 
+    else if(array.length = maxItems){
+        console.log("Your basket is full!");
+        return true;
+    } // end it's full 
+    else if(array.length < maxItems){
+        console.log("You have room in your basket");
+        return false;
+    } // end extra room
+} // end itsFull function
+
+// itsFull function testing.
+console.log("*** - TEST - for function itsFull. ***");
+console.log(itsFull(basket)); // empty array 
+console.log(itsFull(maxItems)); // items full in array
